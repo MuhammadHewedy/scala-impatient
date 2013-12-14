0d7e70cf368dbbd8631b757057521709f011b725
Muhammad Hewedy <mohammed_a_hewedy@hotmail.com>
2013-12-14 17:10:27 +0300

    remove un-necessary code

--- ch03/2.scala
+++ ch03/2.scala
@@ -4,12 +4,11 @@ Array(1, 2, 3, 4, 5)becomes Array(2, 1, 4, 3, 5).
 */
 
 def odd(n: Int) = n % 2 != 0
-def nonZeroOdd(n: Int) = n > 0 && odd(n)
 
 val arr = Array(1, 2, 3, 4, 5, 6, 7, 8)
 var tmp = 0
 
-for (i <- 0 until arr.length if nonZeroOdd(i) ) {
+for (i <- 0 until arr.length if odd(i) ) {
 		tmp = arr(i)
 		arr(i) = arr(i-1)
 		arr(i-1) = tmp
